8dc091a4900de72b98584eec155e626bb2e07248
Maksym Sditanov <msditanov@200volts.com>
2024-11-01 19:58:12 +0200

    Add submit.py
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Maksym Sditanov <msditanov@200volts.com>

--- blenderperf.py
+++ blenderperf.py
@@ -8,23 +8,66 @@ from urllib.parse import urljoin
 def get_scene_report(scene_dir):
     pass
 
-def get_assets_list(outdir):
-    file_name = os.path.join(outdir, 'assets.txt')
-
-    assets = []
-    with open(file_name, "r") as file:
-        file_content = file.readline().strip()
-        assets.append(file_content)
-
-    return assets
-
 class Api:
     url:str = ''
     api_key:str = ''
 
+    __key_api:str = 'api_key'
+    __ping_counter = 0
+
     def __init__(self, url, api_key):
         self.url = url
         self.api_key = api_key
+
+    def ping(self):
+        url = urljoin(self.url, 'gpuperf/nodeapi/ping')
+
+        self.__ping_counter = self.__ping_counter + 1
+
+        data = {
+            self.__key_api: self.api_key,
+            'counter': self.__ping_counter,
+        }
+
+        response = requests.post(url, json.dumps(data))
+        response.raise_for_status()
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print("Failed:", response.status_code, response.message)
+            raise Exception(response.message)
+    
+    def get_node_jobs(self):
+        url = urljoin(self.url, 'gpuperf/nodeapi/jobs')
+        data = {
+            self.__key_api: self.api_key
+        }
+        response = requests.post(url, json.dumps(data))
+        response.raise_for_status()
+
+        if response.status_code == 200:
+            return response.json().get("jobs")
+        else:
+            print("Failed:", response.status_code, response.message)
+            raise Exception(response.message)
+
+    def get_node_tasks(self, job_id):
+        url = urljoin(self.url, 'gpuperf/nodeapi/tasks')
+        data = {
+            self.__key_api: self.api_key,
+            'job_id': job_id,
+        }
+        response = requests.post(url, json.dumps(data))
+        response.raise_for_status()
+
+        if response.status_code == 200:
+            return response.json().get('tasks')
+        else:
+            print("Failed:", response.status_code, response.message)
+            raise Exception(response.message)
+
+
 
     def get_application_hash(self, application_name:str):
         return ''
@@ -110,12 +153,13 @@ def api_create_gpu_report(url, api_key, report_id, asset_hash):
 def main():
     parser = argparse.ArgumentParser(
         prog='submit.py',
-        description='e test for application',
+        description='test for application',
         epilog='for more information: https://github.com/feniksa/blenderperf')
 
     default_url = 'http://localhost:8000'
     default_outdir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'outdir')
 
+    parser.add_argument('-p', '--ping', action='store_true', dest='ping', help='ping server')
     parser.add_argument('-u', '--url', default=default_url, type=str, help='url for upload')
     parser.add_argument('-a', '--apikey', default='qqq', type=str, help='api token')
     parser.add_argument('-o', '--outdir', default=default_outdir, type=str, help='default outdir with metrics')
@@ -124,10 +168,27 @@ def main():
 
     #get_assets_list(args.outdir)
 
-    report_id = api_create_report(args.url, args.apikey, 'blender', '123')
-    print(report_id)
+    api = Api(args.url, args.apikey)
 
-    gpu_report_id = api_create_gpu_report(args.url, report_id)
+    if args.ping:
+        reply = api.ping()
+        print(reply)
+        return 0
+
+    jobs = api.get_node_jobs()
+    for job in jobs:
+        #print(job)
+        job_id = job.get("id")
+        tasks = api.get_node_tasks(job_id)
+        for task in tasks:
+            print(task)
+            print('\n')
+    
+
+    #report_id = api_create_report(args.url, args.apikey, 'blender', '123')
+    #print(report_id)
+
+    #gpu_report_id = api_create_gpu_report(args.url, report_id)
     
     
 if __name__ == "__main__":
